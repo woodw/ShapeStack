dbc810be0d1012afc949f67f9ca187d4fcd615be
bill wood <billwood.dev@gmail.com>
2016-06-01 00:09:37 -0400

    Added Square object with Test to verify surface area is calculated correctly

--- src/main/shape/Square.java
+++ src/main/shape/Square.java
@@ -1,8 +1,19 @@
 package shape;
 
 public class Square {
+  private double shapeArea;
+  private int equiSide;
 
-    public static void main(String[] args) {
-	      // write your code here
-    }
+  public Square(int sideOne) {
+    equiSide = sideOne;
+    setArea();
+  }
+
+  private void setArea(){
+    shapeArea = Math.pow(equiSide,2);
+  }
+
+  public double getArea() {
+    return shapeArea;
+  }
 }
---
+++ src/test/shape/SquareTest.java
@@ -0,0 +1,24 @@
+package shape;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by billwood on 5/31/16.
+ */
+public class SquareTest {
+  public Square mockSquare;
+
+  @Before
+  public void setUp() throws Exception {
+    mockSquare = new Square(4);
+  }
+
+  @Test
+  public void testAreaValue(){
+    assertEquals(16.0,mockSquare.getArea(),0);
+  }
+
+}
