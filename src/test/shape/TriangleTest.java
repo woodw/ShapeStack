b7c13b8a81e50d45fa11b050bd624eb3d1d3a6c7
bill wood <billwood.dev@gmail.com>
2016-06-01 23:58:38 -0400

    Remove beforeTest setup since I only use it for one assertion.

--- src/test/shape/TriangleTest.java
+++ src/test/shape/TriangleTest.java
@@ -12,11 +12,6 @@ import static org.junit.Assert.assertEquals;
 public class TriangleTest {
   public Triangle mockTriangle;
 
-  @Before
-  public void setUp() throws Exception {
-    mockTriangle = new Triangle(4, 10);
-  }
-
   @Test(expected=IllegalArgumentException.class)
   public void testConstructorIllegalArgumentException() {
     mockTriangle = new Triangle(10, 4);
@@ -29,6 +24,7 @@ public class TriangleTest {
 
   @Test
   public void testAreaValue(){
+    mockTriangle = new Triangle(4, 10);
     assertEquals(19.595917942265423,mockTriangle.getArea(),0);
     mockTriangle = new Triangle(6, 9);
     assertEquals(25.45584412271571,mockTriangle.getArea(),0);
