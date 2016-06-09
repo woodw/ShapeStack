c1721cbe07d583e176a7874d048f745a60037d63
bill wood <billwood.dev@gmail.com>
2016-06-08 21:47:33 -0400

    Remove Hypotenuse check from Unit test. assertion can not test failure.

--- src/test/shape/TriangleTest.java
+++ src/test/shape/TriangleTest.java
@@ -13,17 +13,19 @@ public class TriangleTest {
   private Triangle underTest;
 
   @Test(expected=IllegalArgumentException.class)
-  public void testConstructorIllegalArgumentException() {
+  public void shouldThrowExceptionForSmallerHypotenuse() {
     underTest = new Triangle(10, 4);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void testConstructorNegativeIllegalArgumentException() {
+  public void shouldThrowExceptionForNegativeBase() {
     underTest = new Triangle(-4, 9);
+    // Hypotenuse must be greater than base. Base can not be negative. Negative Hypotenuse Check will never hit.
+    //underTest = new Triangle(4, -9);
   }
 
   @Test
-  public void testAreaValue(){
+  public void shouldReturnCorrectCalculatedArea(){
     underTest = new Triangle(4, 10);
     assertEquals(19.595917942265423,underTest.getArea(),0);
     underTest = new Triangle(6, 9);
